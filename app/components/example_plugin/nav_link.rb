f4bc4c81c06da5f05797fa29debee1aa9195c1bd
James Smith <james@floppy.org.uk>
2026-06-25 16:06:26 +0100

    add example nav link component

---
+++ app/components/example_plugin/nav_link.rb
@@ -0,0 +1,9 @@
+class Components::ExamplePlugin::NavLink < Components::Base
+  def view_template
+    a href: "/example_plugin", class: "nav-link" do
+      Icon(icon: "window")
+      whitespace
+      span { t("components.example_plugin.nav_link.label") }
+    end
+  end
+end
