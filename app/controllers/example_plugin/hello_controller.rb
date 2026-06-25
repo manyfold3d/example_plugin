4d02cea227d1c397133fc7821aa2ccdba15fbd22
James Smith <james@floppy.org.uk>
2026-06-25 09:48:58 +0100

    Add some usage example for application data and components

--- app/controllers/example_plugin/hello_controller.rb
+++ app/controllers/example_plugin/hello_controller.rb
@@ -1,6 +1,8 @@
 module ExamplePlugin
   class HelloController < ApplicationController
     def index
+      # Use policy_scope when fetching objects to automatically apply permission checks
+      @models = policy_scope(Model)
     end
   end
 end
