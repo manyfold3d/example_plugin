830d443e65504ed298f388a71ad54d8e144cec65
James Smith <james@floppy.org.uk>
2026-06-29 12:44:49 +0100

    formatting for code style

--- config/initializers/media_types.rb
+++ config/initializers/media_types.rb
@@ -1,5 +1,6 @@
 # Add a new media type with the IANA type, the extension/symbol, and category
-MediaType.register("model/vnd.example", :example3d, category: :model,
+MediaType.register(
+  "model/vnd.example", :example3d, category: :model,
   # These are optional, if required
   additional_extensions: ["ex3d"],
   additional_types: ["model/x-example"]
